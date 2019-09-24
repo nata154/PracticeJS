9136421c376bb16bf4d02b524463323282fdba7c
Natallia Karneyeva1 <Natallia_Karneyeva1@epam.com>
2019-09-24 12:44:16 +0300

    let changes

--- Trolls.js
+++ Trolls.js
@@ -4,10 +4,10 @@ const origPhrase = "This website is for losers LOL!";
 const vowelsToDelete = "aeiouy";
 
 console.log("removeVowelsAsArray---------------------------------------------------------------")
-function removeVowelsAsArray(phrase, lettersToDelete) {
+function removeVowelsAsArray(origPhrase, vowelsToDelete) {
    let newLine = [""];
-   var phrase = origPhrase.split("");
-   var lettersToDelete = vowelsToDelete.split("");
+   let phrase = origPhrase.split("");
+   let lettersToDelete = vowelsToDelete.split("");
    for (let i = 0; i < phrase.length; i++) {
       if (phrase[i] !== "i") {
          if (phrase[i] !== "e") {
@@ -27,29 +27,28 @@ console.log(removeVowelsAsArray(origPhrase, vowelsToDelete));
 console.log("---------------------------------------------------------------")
 
 console.log("removeVowelsInString---------------------------------------------------------------")
-function removeVowelsInString(phrase2, lettersToDelete) {
-   var phrase2 = origPhrase;
-   var lettersToDelete2 = lettersToDelete.split("");
+function removeVowelsInString(origPhrase, vowelsToDelete) {
+   let phrase2 = origPhrase;
+   let lettersToDelete2 = vowelsToDelete.split("");
    for (let i = 0; i < lettersToDelete2.length; i++) {
-      var s = origPhrase.replace("o", "&").replace("i", "&");
+      var s = phrase2.replace("o", "&").replace("i", "&");
       //var s2 = s.search(/[TL]/g, "#").replace(); //dont work, only searchs
       return s;
    }
-
 }
 console.log(removeVowelsInString(origPhrase, vowelsToDelete));
 console.log("---------------------------------------------------------------")
 
 console.log("phraseAsWords.map(x => x.replace(/[aeiouyAEIOUY]/g, '')---------------------------------------------------------------")
 let phraseAsWords = origPhrase.split(" ");
-var string = phraseAsWords.map(x => x.replace(/[aeiouyAEIOUY]/g, ''));
+let string = phraseAsWords.map(x => x.replace(/[aeiouyAEIOUY]/g, ''));
 console.log(string);
 console.log("---------------------------------------------------------------")
 
 console.log("origPhrase.replace(o  & )---------------------------------------------------------------")
 //var str = origPhrase.search(/[aeiouyAEIOUY]/g).replace('');  don't work cause it is string
 //console.log(str);
-var s = origPhrase.replace("o", "&");
+let s = origPhrase.replace("o", "&");
 console.log(s);
 console.log("---------------------------------------------------------------")
 
